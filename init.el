9219a3e723f75ad56c372cd513b5dabe89f63ab0
somura <c1100921@planet.kanazawa-it.ac.jp>
2024-09-11 12:19:56 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,35 @@
+;;; init.el --- The configure of my emacs
+;;; Commentary:
+;;; Code:
+(require 'package)
+
+
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-journal eww-lnum random-splash-image vterm doom-modeline parrot flycheck-rust all-the-icons neotree treemacs-magit treemacs-icons-dired treemacs-projectile treemacs nerd-icons aggressive-indent nix-mode rustic cargo rust-mode T tree-sitter-langs tree-sitter magit lsp-ivy counsel ivy nyan-mode spaceline smartparens python-mode lsp-ui flycheck ddskk corfu catppuccin-theme cape)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
